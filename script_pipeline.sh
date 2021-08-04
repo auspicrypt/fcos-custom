e8b3288f7ceb45f165b1fc3a208f24320ccff19d
Shubham Saroha <33714517+auspicrypt@users.noreply.github.com>
2021-08-04 09:28:55 +0200

    Files added to produce ISO
    Pipeline script and sample butane, ignition file added

---
+++ script_pipeline.sh
@@ -0,0 +1,20 @@
+#1/bin/bash
+
+echo "Enter butane filename to convert"
+
+#Convert butane file to ignition file
+
+read bufile
+
+podman run --rm --tty --interactive --security-opt label=disable --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release --pretty --strict $bufile --output ignition_out.ign
+
+#Validate ignition file
+
+podman run --rm --tty --interactive --security-opt label=disable --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/ignition-validate:release ignition_out.ign && echo 'Congrats!! Ignition file validated.'
+
+#convert ignition file to iso
+
+echo "Enter ignition filename to convert"
+
+podman run --rm --tty --interactive --security-opt label=disable --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/coreos-installer :release iso ignition embed -ignition-file ignition_out.ign --output custom_fcos.iso
+
