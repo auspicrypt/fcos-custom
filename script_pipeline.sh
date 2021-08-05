06e6e596c27945593412ccfb6fc3ebbf0066dbc0
auspicrypt <saroha93@gmail.com>
2021-08-05 10:08:12 +0200

    added echo command to know if the ISO command was executed

--- script_pipeline.sh
+++ script_pipeline.sh
@@ -18,3 +18,5 @@ echo "Enter ignition filename to convert"
 
 podman run --rm --tty --interactive --security-opt label=disable --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/coreos-installer:release iso ignition embed fedora-coreos.iso --ignition-file ignition_out.ign --output custom_fcos.iso
 
+
+echo "ISO file created!!"
